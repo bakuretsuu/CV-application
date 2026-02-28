df913709e326a0df7c1d27b31a1299aee614c4cf
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2026-02-28 09:27:29 +0800

    added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 import GeneralInfo from "./components/GeneralInfo";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
+import "./App.css";
+import "./styles/section.css";
 
 function App() {
   return (
